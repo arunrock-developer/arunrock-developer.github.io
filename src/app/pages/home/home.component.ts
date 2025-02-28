593079ea1570d8cc39fa8e6ec603b22a2aeac80b
Arun Kumar <arunrock1264708@gmail.com>
2025-02-28 19:00:29 +0530

    home page almost finish

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import {  Component, OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  words: string[] = ["Developer", "Designer", "App Developer"];
+
+export class HomeComponent implements OnInit  {
+  words: string[] = [
+    "Angular Developer",
+    "Frontend Developer",
+    "Figma Designer",
+    "Flutter Developer",
+  ];
+
+  
+
+
+
+  aboutText=[
+  {
+    "option":"Full Name",
+    "answer":"AunKumar R"
+  },
+  {
+    "option":"DOB",
+    "answer":"06 June 2002  "
+  },
+  {
+    "option":"Experience",
+    "answer":"2.5 years"
+  },
+  {
+    "option":"Address",
+    "answer":"Tirupur"
+  },
+  {
+    "option":"Freelance",
+    "answer":"Available"
+  },
+  {
+    "option":"Phone",
+    "answer":"+91 96988 77357"
+  },
+  {
+    "option":"Email",
+    "answer":"arunrock1264708@gmail.com"
+  },
+  ]
+  
   currentWord: string = this.words[0];
   wordIndex: number = 0;
   
@@ -14,6 +58,7 @@ export class HomeComponent {
   
   ngOnInit(): void {
     this.startTypingEffect();
+
   }
   
   startTypingEffect(): void {
@@ -22,5 +67,8 @@ export class HomeComponent {
       this.currentWord = this.words[this.wordIndex];
     }, 6000); // Change every 3 seconds to match animation
   }
+
+
+  
   
 }
